dcf9a3a5266bb41d6f4697ff40dc5cdb72a7c9b5
rdfornieles <rdfornieles@gmail.com>
2018-12-17 16:30:23 +0100

    done function handler

--- _src/assets/js/partials/submit.js
+++ _src/assets/js/partials/submit.js
@@ -1,1 +1,21 @@
-console.log('hola');
+const btnEl = document.querySelector(".btn");
+
+const input = document.querySelector("#searchTv");
+
+const listEl = document.querySelector(".list");
+
+function handlerSearch(event){
+    const inputEl = input.value;
+    event.preventDefault();
+
+    fetch(` http://api.tvmaze.com/search/shows?q=${inputEl}`)
+    .then(function(response) {
+        return response.json();
+      })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.name);
+    })
+}
+
+btnEl.addEventListener('click', handlerSearch);
