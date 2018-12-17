27b4b44fa7d75c19cf7684f627d497ce60069990
rdfornieles <rdfornieles@gmail.com>
2018-12-17 17:22:22 +0100

    done for

--- _src/assets/js/partials/submit.js
+++ _src/assets/js/partials/submit.js
@@ -14,7 +14,15 @@ function handlerSearch(event){
       })
     .then(function(data) {
         console.log(data);
-        console.log(data.show);
+        
+        for ( const itemData of data) {
+            const itemDataName = itemData.show.name;
+            const itemDataImageM = itemData.show.image.medium;
+            const itemDataImageO = itemData.show.image.original;
+            console.log(itemDataImageO);
+            console.log(itemDataImageM);
+            listEl.innerHTML += `<li class="listEl"> ${itemDataName} ${itemDataImageM} ${itemDataImageO}</li>`;
+        }
     })
 }
 
