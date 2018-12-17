ecfe2dbbb546edc0d48a1c254f4dd684413f0cab
rdfornieles <rdfornieles@gmail.com>
2018-12-17 21:00:29 +0100

    not fuction favorite

--- _src/assets/js/partials/submit.js
+++ _src/assets/js/partials/submit.js
@@ -20,7 +20,6 @@ function handlerSearch(event){
             let itemDataImage = itemData.show.image;
 
             const imageNull = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
-            console.log(imageNull);
             
             if (itemDataImage === null) {
                 itemDataImage = imageNull
@@ -28,18 +27,24 @@ function handlerSearch(event){
                 itemDataImage = itemData.show.image.medium
             }
 
-            listEl.innerHTML += `<img src="${itemDataImage}" alt="Imagen de la serie"></img><li class="listEl"> ${itemDataName} </li>`;
+            listEl.innerHTML += `<li class="liEl"><img src="${itemDataImage}" alt="Imagen de la serie"></img> ${itemDataName} </li>`;
         }
         
 
     })
-    function favoriteShow(event) {
-        const selectFavorite = event.target;
-        selectFavorite.classList.toggle("favorite");
-        console.log(favoriteShow);
-    }
-    listEl.addEventListener('click', favoriteShow);
+    
+
 }
+function favoriteShow(event) {
+    const selectFavorite = event.currentTarget;
+    selectFavorite.classList.toggle("favorite");
 
-
+   
+}
 btnEl.addEventListener('click', handlerSearch);
+listEl.addEventListener('click', favoriteShow);
+
+/*function printLocalStorage() {
+    localStorage.setItem(itemData.show.id);
+}
+printLocalStorage();*/
